c867ed030005056224409e0bdc82ec4a365b66b7
David Kowis <david@kow.is>
2020-12-31 15:05:35 -0600

    Applying recommended best practice for project conventions
    https://docs.gradle.org/current/userguide/sharing_build_logic_between_subprojects.html
    
    Apparently you should not use the `submodules` thing any more. This
    isn't too bad.

--- test-clock-core/build.gradle.kts
+++ test-clock-core/build.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+  id("test-clock-conventions")
+}
 
 dependencies {
   implementation("com.google.code.findbugs:jsr305:3.0.2")
--- test-clock-kotlin/build.gradle.kts
+++ test-clock-kotlin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.21"
+    id("test-clock-conventions")
 }
 repositories {
     mavenCentral()
